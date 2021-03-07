7cfae3bea9a5056cd49061a490601e1e8cccdef2
sumeikom <sumeikom@gmail.com>
2021-03-06 20:00:44 -0500

    commit tree 4ee7ade512050739aa6cd44fa5cede61307bb196

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+# describe "prime?" do
+# it 'returns true for prime numbers' do
+# it 'returns false for non-prime numbers' do
+# .negative?
+# (n < 0) 
+
+def prime?(number)
+
+    start = 2
+        
+        if number > 1
+        
+        range = (start..number-1).to_a
+        range.none? do |num_to_test|
+        number % num_to_test == 0
+end
+    else false
+        
+end
+end
+    
